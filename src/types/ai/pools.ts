cf67d426fe7bb9e28d833120cc8281e1ea242775
0xdeval <26190360+0xdeval@users.noreply.github.com>
2025-08-02 16:50:32 +0100

    fix

--- src/types/ai/pools.ts
+++ src/types/ai/pools.ts
@@ -1,6 +1,10 @@
 export interface PoolRecommendation {
   poolId: string;
   feeTier: string;
+  token0Symbol: string;
+  token1Symbol: string;
+  token0Name: string;
+  token1Name: string;
   token0: string;
   token1: string;
   isStablecoinPool: boolean;
